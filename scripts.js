7a3ff775520fa993cbafaf8b20b6775265b99686
Adam Lezotte <adam@lezotte.me>
2017-03-08 21:05:02 -0500

    task names

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ var app = angular.module("toDoList", []);
 app.controller('listCtrl', function($scope){
     
     $scope.tasks = [
-        {text:'task1', done:false},
-        {text:'task2', done:false},
-        {text:'task3', done:false}
+        {text:'Task 1', done:false},
+        {text:'Task 2', done:false},
+        {text:'Task 3', done:false}
     ];
     
     $scope.addTask = function(){
@@ -18,7 +18,7 @@ app.controller('listCtrl', function($scope){
         $scope.tasks = [];
         angular.forEach(oldTasks, function(task) {
             if (!task.done){
-                $scope.tasks.push(task)
+                $scope.tasks.push(task);
             }
         });
     };
