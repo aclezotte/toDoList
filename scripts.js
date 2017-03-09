4ff984742482564cb2b3809a05991d90801dbbf5
Adam Lezotte <adam@lezotte.me>
2017-03-08 21:00:13 -0500

    task list with strikethrough

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
-var app = angular.module('toDoList', []);
+var app = angular.module("toDoList", []);
 
 app.controller('listCtrl', function($scope){
     
-    $scope.tasks = [];
+    $scope.tasks = [
+        {text:'task1', done:false},
+        {text:'task2', done:false},
+        {text:'task3', done:false}
+    ];
     
     $scope.addTask = function(){
-        $scope.tasks.push($scope.newTask);
+        $scope.tasks.push({text:$scope.newTask, done:false});
         $scope.newTask = "";
+    };
+    
+    $scope.checkOff = function() {
+        var oldTasks = $scope.tasks;
+        $scope.tasks = [];
+        angular.forEach(oldTasks, function(task) {
+            if (!task.done){
+                $scope.tasks.push(task)
+            }
+        });
     };
     
 });
